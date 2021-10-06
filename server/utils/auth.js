8b4e63f696f2f13f42efa1b330e02f12a0324949
benjaminbtrance <benjaminb.tran@gmail.com>
2021-10-06 00:44:01 -0500

    Update the auth middleware function to work with the GraphQL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,7 +8,7 @@ module.exports = {
 	// function for our authenticated routes
 	authMiddleware: function ({ req }) {
 		// allows token to be sent via  req.query or headers
-		let token = req.query.token || req.query.token || req.headers.authorization;
+		let token = req.body.token || req.query.token || req.headers.authorization;
 
 		// ["Bearer", "<tokenvalue>"]
 		if (req.headers.authorization) {
@@ -26,6 +26,9 @@ module.exports = {
 		} catch {
 			console.log('Invalid token');
 		}
+
+		// return updated request object
+		return req;
 
 		// send to next endpoint
 		// next();
